67d7320e8ba9e6006c34d5b9a4b399f551883f3b
Natashia White <whitenat@oregonstate.edu>
2018-11-28 11:34:02 -0800

    G5 graph added

--- main.js
+++ main.js
@@ -15,6 +15,10 @@ let yearsDrake = []
 
 let monthsDrake = []
 
+let yearsG5 = []
+
+let monthsG5 = []
+
 //Pulls data from JQUERY request and ensures order of execution
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count", updateRideCount)
@@ -36,6 +40,12 @@ function updateView() {
 
     $.when (perMonthsDrake
     ).then(updatePerMonthDrake);
+
+    $.when ($.getJSON(BASE_URL + "/rides/count/g5/per_month", perYearG5),
+    ).then(perMonthsG5);
+
+    $.when (perMonthsG5
+    ).then(updatePerMonthG5);
 
 }
 
@@ -76,6 +86,17 @@ function perYearDrake(data) {
         }
     }
     console.log(yearsDrake);
+}
+
+function perYearG5(data) {
+    for (var i = 2016; i <= 2018; ++i){
+        if (data[i] === undefined) {
+            yearsG5.push(0)
+        } else{
+        yearsG5.push(data[i]);
+        }
+    }
+    console.log(yearsG5);
 }
 
 //pulls the rides per month and pushes it into the months array
@@ -125,6 +146,22 @@ function perMonthsDrake() {
         } else {
             monthsDrake.push(yearsDrake[2][m][y]);
         }
+    }
+}
+
+function perMonthsG5() {
+    for (var m = 0, y = 9; m <=9, y <= 18; ++m, ++y) {
+        if (yearsG5[0] === 0){
+            monthsG5.push(0);
+        } else {
+            monthsG5.push(yearsG5[0][m][y]);
+        }
+    }
+    for (var m = 0, y = 6; m <=1, y <= 12; ++m, ++y) {
+            monthsG5.push(yearsG5[1][m][y]);
+    }
+    for (var m = 0, y = 1; m <=9, y <= 10; ++m, ++y) {
+            monthsG5.push(yearsG5[2][m][y]);
     }
 }
 
@@ -192,7 +229,7 @@ function updatePerMonthDrake() {
         data: {
             labels: [ "9/16", "10/16", "11/16", "12/16", "1/17", "2/17", "3/17", "4/17", "5/17", "6/17", "7/17", "8/17", "9/17", "10/17", "11/17", "12/17", "1/18", "2/18", "3/18", "4/18", "5/18", "6/18", "7/18", "8/18","9/18", "10/18" ],
             datasets: [{
-                label: "Zagster Rides Per Month at Drake",
+                label: "Zagster Rides Per Month at Drake Park",
                 backgroundColor: '#3f3f3f',
                 borderColor: '#FC4A1A',
                 data: monthsDrake,
@@ -203,4 +240,28 @@ function updatePerMonthDrake() {
         options: {}
     });
 }
- 
+
+function updatePerMonthG5() {
+
+    console.log(monthsG5)
+
+    var ctx = document.getElementById('monthlyG5Chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+    
+        // The data for our dataset
+        data: {
+            labels: [ "9/16", "10/16", "11/16", "12/16", "1/17", "2/17", "3/17", "4/17", "5/17", "6/17", "7/17", "8/17", "9/17", "10/17", "11/17", "12/17", "1/18", "2/18", "3/18", "4/18", "5/18", "6/18", "7/18", "8/18","9/18", "10/18" ],
+            datasets: [{
+                label: "Zagster Rides Per Month at G5",
+                backgroundColor: '#3f3f3f',
+                borderColor: '#FC4A1A',
+                data: monthsG5,
+            }]
+        },
+    
+        // Configuration options go here
+        options: {}
+    });
+}
